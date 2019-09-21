45537ed071bb211a44f769643ebf5d6852bb5a95
Robin Klaus <rmklaus12@gmail.com>
2019-09-21 13:48:48 +1000

    added move functionality for all directions. tests added as well

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,26 +1,37 @@
 module ToyRobot
-    class Robot
-        attr_reader :east, :north
+  class Robot
+    attr_reader :east, :north
 
-        def initialize(east = 0, north = 0)
-            @east = east
-            @north = north
-        end
-
-        def move_east
-            @east += 1
-        end
-
-        def move_west
-            @east -= 1
-        end
-
-        def move_north
-            @north += 1
-        end
-
-        def move_south
-            @north -= 1
-        end
+    def initialize(east = 0, north = 0, direction = 'north')
+      @east = east
+      @north = north
+      @direction = direction
     end
+
+    def move
+      send("move_#{@direction.downcase}")
+      # case @direction
+      # when "north" then move_north
+      # when "south" then move_south
+      # when "east" then move_east
+      # when "west" then move_west
+      # end
+    end
+
+    def move_east
+      @east += 1
+    end
+
+    def move_west
+      @east -= 1
+    end
+
+    def move_north
+      @north += 1
+    end
+
+    def move_south
+      @north -= 1
+    end
+  end
 end
--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -1,45 +1,81 @@
-require 'spec_helper'
+    require 'spec_helper'
 
-RSpec.describe ToyRobot::Robot do
-    subject { ToyRobot::Robot.new(0) }
+    RSpec.describe ToyRobot::Robot do
+      subject { ToyRobot::Robot.new(0) }
 
-    it "moves 3 spaces east" do
+      it 'moves 3 spaces east' do
         3.times { subject.move_east }
         expect(subject.east).to eq(3)
-    end
+      end
 
-    it "moves 4 spaces east" do
+      it 'moves 4 spaces east' do
         4.times { subject.move_east }
         expect(subject.east).to eq(4)
-    end
+      end
 
-    it "moves 3 spaces west" do
+      it 'moves 3 spaces west' do
         3.times { subject.move_west }
         expect(subject.east).to eq(-3)
-    end
+      end
 
-    it "moves 4 spaces west" do
+      it 'moves 4 spaces west' do
         4.times { subject.move_west }
         expect(subject.east).to eq(-4)
-    end
+      end
 
-    it "moves 3 spaces north" do
+      it 'moves 3 spaces north' do
         3.times { subject.move_north }
         expect(subject.north).to eq(3)
-    end
+      end
 
-    it "moves 4 spaces north" do
+      it 'moves 4 spaces north' do
         4.times { subject.move_north }
         expect(subject.north).to eq(4)
-    end
+      end
 
-    it "moves 3 spaces south" do
+      it 'moves 3 spaces south' do
         3.times { subject.move_south }
         expect(subject.north).to eq(-3)
-    end
+      end
 
-    it "moves 4 spaces south" do
+      it 'moves 4 spaces south' do
         4.times { subject.move_south }
         expect(subject.north).to eq(-4)
-    end
-end
+      end
+
+      context 'when facing north' do
+        subject { ToyRobot::Robot.new(0, 0, 'north') }
+
+        it 'moves north' do
+          subject.move
+          expect(subject.north).to eq(1)
+        end
+      end
+
+      context 'when facing south' do
+        subject { ToyRobot::Robot.new(0, 0, 'south') }
+
+        it 'moves south' do
+          subject.move
+          expect(subject.north).to eq(-1)
+        end
+      end
+
+      context "when facing east" do
+        subject { ToyRobot::Robot.new(0, 0, 'east') }
+
+        it 'moves east' do
+          subject.move
+          expect(subject.east).to eq(1)
+        end
+      end
+
+      context 'when facing west' do
+        subject { ToyRobot::Robot.new(0, 0, 'west') }
+
+        it 'moves west' do
+          subject.move
+          expect(subject.east).to eq(-1)
+        end
+      end
+  end
