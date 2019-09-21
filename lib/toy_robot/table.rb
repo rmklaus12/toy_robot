7767a7de4f859d843d8d071852e3dd177c7d1546
Robin Klaus <rmklaus12@gmail.com>
2019-09-21 16:41:26 +1000

    added table class to validate location

---
+++ lib/toy_robot/table.rb
@@ -0,0 +1,15 @@
+module ToyRobot
+  class Table
+    def initialize(width, length)
+      @width = width
+      @length = length
+    end
+
+    def valid_location? (east, north)
+      (0...@width).cover?(east) &&
+      (0...@length).cover?(north)
+      # east >= 0 && east < @width &&
+      # north >= 0 && north < @length
+    end
+  end
+end
